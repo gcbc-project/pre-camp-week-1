2f1c7b9a2fe141cfe784faeae80b739c90c3966e
l11040 <113540683+l11040@users.noreply.github.com>
2024-03-21 14:23:13 +0900

    Update : 랜덤 생성 빗방울

--- Assets/Scripts/Rain.cs
+++ Assets/Scripts/Rain.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class Rain : MonoBehaviour
 {
+    float size = 1.0f;
+    int score = 1;
+    SpriteRenderer rainSprite;
+
     // Start is called before the first frame update
     void Start()
     {
+        // 비 오브젝트의 SpriteRenderer Inspector 정보를 가져오기
+        rainSprite = GetComponent<SpriteRenderer>();
 
+        // 비오브젝트의 위치를 랜덤으로 생성 (X,Y 각각)
+        float poX = Random.Range(-2.4f, 2.4f);
+        float poY = Random.Range(3.0f, 5.0f);
+
+        // 비 오브젝트의 위치 이동
+        this.transform.position = new Vector3(poX, poY, 0);
+
+        // 비 오브젝트의 타입을 랜덤으로 생성 (1~3) 정수형
+        int rainType = Random.Range(1, 4);
+
+        if (rainType == 1)
+        {
+            size = 0.8f;
+            score = 1;
+            rainSprite.color = new Color(100 / 255f, 100 / 255f, 1f, 1f);
+        }
+        else if (rainType == 2)
+        {
+            size = 1.0f;
+            score = 2;
+            rainSprite.color = new Color(130 / 255f, 130 / 255f, 1f, 1f);
+        }
+        else if (rainType == 3)
+        {
+            size = 1.2f;
+            score = 3;
+            rainSprite.color = new Color(150 / 255f, 150 / 255f, 1f, 1f);
+        }
+
+        // 비 오브젝트의 크기를 변경
+        transform.localScale = new Vector3(size, size, 0);
     }
 
     // Update is called once per frame
